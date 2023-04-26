61b862267178147cd0963dfd546843682caf1e0e
Buğrahan Özdemir <bugrahan_ozdemir@outlook.com>
2023-04-27 00:00:14 +0300

    added "text to nato alphabet"

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import TextToNatoAlphabet from "./pages/TextToNatoAlphabet"
 const App = () => {
   return (
     <div className='min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased'>
-      <div className='py-5 px-72'>
+      <div className='py-6 px-72'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route
